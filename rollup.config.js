3d8d5ea1298786953eb6081378db8d5ba5fdbfa0
itihon <kalabin.alexandr@gmail.com>
2025-03-27 21:07:17 +0500

    build: add rollup plugin replace

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import replace from '@rollup/plugin-replace';
 
 export default {
-  plugins: [nodeResolve()],
+  plugins: [ nodeResolve(), replace({'#': '__' }) ],
 };
