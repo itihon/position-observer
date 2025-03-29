1f69a1e8f6876fef4aa31f44f8982b435340f006
itihon <kalabin.alexandr@gmail.com>
2025-03-29 12:43:42 +0500

    build: add build:demo:deploy script

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,18 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
+const importResplace = 
+  process.env.BUILD === 'deploy' 
+    ? { 
+      'import PositionObserver from \'../../lib/index.js\';': 
+        'import PositionObserver from \'@itihon/position-observer\';'
+    } 
+    : undefined;
+
 export default {
   plugins: [
     replace({ 
-      values: { '#': '__' }, 
+      values: { '#': '__', ...importResplace }, 
       preventAssignment: true,
       delimiters: ['', ''],
     }), 
