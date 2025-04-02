b563e93f9221441ac4399b724422ccafa28e6b8f
itihon <kalabin.alexandr@gmail.com>
2025-04-02 11:36:44 +0500

    refactor(work-visualization.js): change according to new library version

--- demo/src/work-visualization.js
+++ demo/src/work-visualization.js
@@ -1,9 +1,5 @@
 import PositionObserver from '../../lib/index.js';
 
-let observerTop; 
-let observerRight;
-let observerBottom;
-let observerLeft;
 let timeout;
 let delay = 500;
 
@@ -37,41 +33,78 @@ const resizeObserverRect = (observerRectElement, rect) => {
   classList.add('recreated');
 };
 
+const runIfNew = (
+  (instances = new Map()) => (key, value, fn, ...args) => {
+    if (instances.get(key) !== value) {
+      fn(...args);
+      instances.set(key, value);
+    }
+  }
+)();
+
 PositionObserver.prototype.__observerCallback = function (entries, self) {
 
   const entry = entries[entries.length - 1];
   const { target } = entry;
   const observers = self.__observers.get(target);
+
+  const takeTopRecords = observers.top.takeRecords;
+  const takeRightRecords = observers.right.takeRecords;
+  const takeBottomRecords = observers.bottom.takeRecords;
+  const takeLeftRecords = observers.left.takeRecords;
+
+  let topRecords;
+  let rightRecords;
+  let bottomRecords;
+  let leftRecords;
   
-  if (this.isFirstCall(target)) {
-    if (this === observers.bottom) {
-      if (this !== observerBottom) {
-        observerBottom = this;
-        resizeObserverRect(observerBottomRect, entry.rootBounds);
-      }
-    } 
-
-    if (this === observers.left) {
-      if (this !== observerLeft) {
-        observerLeft = this;
-        resizeObserverRect(observerLeftRect, entry.rootBounds);
-      }
-    }
-
-    if (this === observers.top) {
-      if (this !== observerTop) {
-        observerTop = this;
-        resizeObserverRect(observerTopRect, entry.rootBounds);
-      }
-    }
-
-    if (this === observers.right) {
-      if (this !== observerRight) {
-        observerRight = this;
-        resizeObserverRect(observerRightRect, entry.rootBounds);
-      }
-    }
+  if (this === observers.top) {
+    runIfNew('top', observers.top, resizeObserverRect, observerTopRect, entry.rootBounds);
   }
+
+  if (this === observers.right) {
+    runIfNew('right', observers.right, resizeObserverRect, observerRightRect, entry.rootBounds);
+  }
+
+  if (this === observers.bottom) {
+    runIfNew('bottom', observers.bottom, resizeObserverRect, observerBottomRect, entry.rootBounds);
+  } 
+
+  if (this === observers.left) {
+    runIfNew('left', observers.left, resizeObserverRect, observerLeftRect, entry.rootBounds);
+  }
+
+  observers.top.takeRecords = () => {
+    topRecords = takeTopRecords.call(observers.top);
+    if (topRecords.length) {
+      runIfNew('top', observers.top, resizeObserverRect, observerTopRect, topRecords[topRecords.length - 1].rootBounds);
+    }
+    return topRecords;
+  };
+  
+  observers.right.takeRecords = () => {
+    rightRecords = takeRightRecords.call(observers.right);
+    if (rightRecords.length) {
+      runIfNew('right', observers.right, resizeObserverRect, observerRightRect, rightRecords[rightRecords.length - 1].rootBounds);
+    }
+    return rightRecords;
+  };
+
+  observers.bottom.takeRecords = () => {
+    bottomRecords = takeBottomRecords.call(observers.bottom);
+    if (bottomRecords.length) {
+      runIfNew('bottom', observers.bottom, resizeObserverRect, observerBottomRect, bottomRecords[bottomRecords.length - 1].rootBounds);
+    }
+    return bottomRecords;
+  };
+
+  observers.left.takeRecords = () => {
+    leftRecords = takeLeftRecords.call(observers.left);
+    if (leftRecords.length) {
+      runIfNew('left', observers.left, resizeObserverRect, observerLeftRect, leftRecords[leftRecords.length - 1].rootBounds);
+    }
+    return leftRecords;
+  };
 
   clearTimeout(timeout);
   timeout = setTimeout(flashed, delay);
@@ -83,11 +116,11 @@ import('./input-position-observer.js');
 
 // const originalCreateObserver = PositionObserver.__createObserver;
 // 
-// PositionObserver.__createObserver = function(target, targetRect, side, observers, self) {
+// PositionObserver.__createObserver = function(ctx) {
 // 
-//   console.log(side);
+//   const { target } = ctx;
 // 
 //   target.parentNode.scroll(1000, 1000);
 // 
-//   originalCreateObserver.call(this, target, targetRect, side, observers, self);
+//   originalCreateObserver.call(this, ctx);
 // }
