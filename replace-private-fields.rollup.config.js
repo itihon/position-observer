c3a736d9c1c50a402a7cd01dc9baa0332014bb24
itihon <kalabin.alexandr@gmail.com>
2025-04-10 15:43:46 +0500

    chore(package.json): add rollup-plugin-import-css

--- replace-private-fields.rollup.config.js
+++ replace-private-fields.rollup.config.js
@@ -1,7 +1,9 @@
 import replace from '@rollup/plugin-replace';
+import css from "rollup-plugin-import-css";
 
 export default {
   plugins: [
+    css(),
     replace({ 
       values: { '#': '__' }, 
       preventAssignment: true,
