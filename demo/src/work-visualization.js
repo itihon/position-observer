4a55645d81ba6a1776d6ff4d8118432c71e2fc3a
itihon <kalabin.alexandr@gmail.com>
2025-10-31 10:34:38 +0500

    fix: remove unsupported transform attribute on svg tag in Epiphany(WebKit)

--- demo/src/work-visualization.js
+++ demo/src/work-visualization.js
@@ -13,7 +13,7 @@ function callback(target, targetRect, ctx) {
   const { tooltip, tooltipHeight } = ctx;
   const { left, top } = targetRect;
   const { scrollLeft, scrollTop } = document.documentElement;
-  const offsetCorner = 90;
+  const offsetCorner = 38;
 
   const x = left - offsetCorner + scrollLeft;
   const y = top - tooltipHeight + scrollTop;
